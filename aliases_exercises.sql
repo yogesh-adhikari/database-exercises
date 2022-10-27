e57524731b04dc02724c86a8ad25cee3e584042e
Yogesh Adhikari <yogeshadhikari81@gmail.com>
2022-10-27 11:07:09 -0500

    aliases exercise

---
+++ aliases_exercises.sql
@@ -0,0 +1,14 @@
+SELECT CONCAT(last_name, ' ' , first_name) AS full_name
+FROM employees
+LIMIT 10
+;
+
+SELECT CONCAT(last_name, ' ' , first_name) AS full_name, birth_date AS DOB
+FROM employees
+LIMIT 10
+;
+
+SELECT CONCAT(emp_no,' - ',last_name, ' ' , first_name) AS full_name, birth_date AS DOB
+FROM employees
+LIMIT 10
+;
