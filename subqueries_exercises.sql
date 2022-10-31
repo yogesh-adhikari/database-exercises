cf57b62be77cc09a71d4ca2ae0898f5957da8fc9
Yogesh Adhikari <yogeshadhikari81@gmail.com>
2022-10-31 13:30:28 -0500

    bonus exercise completed

--- subqueries_exercises.sql
+++ subqueries_exercises.sql
@@ -1,3 +1,4 @@
+-- Employees with same hire date --
 SELECT emp_no, first_name, last_name
 FROM employees
 WHERE hire_date IN (
@@ -6,6 +7,7 @@ WHERE hire_date IN (
     WHERE emp_no = 101010
 );
 
+-- Title held by all employees with same first name --
 SELECT title
 from titles
 where emp_no IN (
@@ -14,13 +16,41 @@ where emp_no IN (
     where first_name = 'Aamod'
     );
 
+
+-- Current female department managers --
 SELECT first_name, last_name
 from employees
 where gender = 'F' and emp_no IN (
     select emp_no
     from dept_manager
     where to_date = '9999-01-01'
-)
+);
 
+-- Bonus --
+-- Department names that currently have female managers --
+select dept_name
+from departments
+where dept_no IN (
+    select dept_no
+    from dept_manager
+    where emp_no in (
+        select emp_no
+        from employees
+        where gender = 'F' and to_date = '9999-01-01'
+        )
+    );
+
+-- Name of the employee with highest salary --
+select first_name, last_name
+from employees
+where emp_no IN (
+    select emp_no
+    from salaries
+    where salary IN (
+        select MAX(salary)
+        from salaries
+        )
+    )
+limit 1;
 
 
