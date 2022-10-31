26532b47eb2cb017ee06de68541a44e437cdf45c
Yogesh Adhikari <yogeshadhikari81@gmail.com>
2022-10-31 12:07:31 -0500

    subqueries exercise completed

--- subqueries_exercises.sql
+++ subqueries_exercises.sql
@@ -14,6 +14,13 @@ where emp_no IN (
     where first_name = 'Aamod'
     );
 
+SELECT first_name, last_name
+from employees
+where gender = 'F' and emp_no IN (
+    select emp_no
+    from dept_manager
+    where to_date = '9999-01-01'
+)
 
 
 
